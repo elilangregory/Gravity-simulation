7fd2bd35c26c2fdebed9df6fe42e12e2f09d81e1
elilangregory <elilan.gregory@gmail.com>
2025-04-07 15:05:19 +0200

    Initial commit med main.cpp, meson.build og build config

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "std_lib_facilities.h"
+
+// C++ programs start by executing the function main
+int main() {
+    // Show a nice message in the Terminal window
+    cout << "Hello!" << endl;
+
+    // This lets the operating system (Windows, Mac, Linux, etc.) know that the program
+    // did not encounter any errors
+    return 0;
+}
+
+//------------------------------------------------------------------------------
